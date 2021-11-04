c0d465d26c9ca386047b4bec24b92535c47e4f00
Crowdbotics <team@crowdbotics.com>
2021-11-04 20:59:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "yellowstone_youth_a_31831",
+  url: "https://yellowstone_youth_a_31831.botics.co",
+  api: "https://yellowstone_youth_a_31831.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
